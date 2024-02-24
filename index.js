a6c0c0de6fe11ca0a97d7822d0e0fec509aba478
moltam89 <moltam89@gmail.com>
2024-02-24 15:12:56 -0700

    Way to log a blindedNumber hash

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const { BigNumber, ethers, utils } = require("ethers");
+
 const main = () => {
 	console.log("yo");
+
+	const number = 1;
+	const secret = "yourSecret"
+
+	const blindedNumber = getKeccak256(number, secret);
+
+	console.log(number, blindedNumber);
+}
+
+const getKeccak256= (number, secret) => {
+  return utils.solidityKeccak256([ "uint256", "string" ], [ number, secret ]);  
 }
 
 main();
